5bf5cfec071da676be9a8af2293c7098252b6c31
Luis Trujillo <Luis_Aguilar303@hotmail.com>
2025-02-19 01:35:27 -0600

    Actualización 1.1

--- wbs.js
+++ wbs.js
@@ -7,7 +7,7 @@ const pswd = "Harman.2024"
 async function OpenPage() {
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 200
+        slowMo: 20,
     })
     const openpage = await browser.newPage()
 
@@ -24,6 +24,49 @@ async function OpenPage() {
     await openpage.click('span[id=ctl00_TopNavigationPanelInventoryManager1_lblInventory]')
     await openpage.click('span[id=ctl00_ContentPlaceHolder1_BtnHistory]')
     await openpage.click('a[id=ctl00_ContentPlaceHolder1_DeliveryHistoryGrid_ctl03_LinkBtnPraxairNo]')
+    await new Promise(resolve => setTimeout(resolve, 5000))
+
+    //solicitud al sistema para recoleción de datos 
+
+    const resultado1 = await openpage.evaluate(() => {
+        const data1 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(2) > td:nth-child(2)")
+        return data1.innerText
+    })
+
+    const resultado2 = await openpage.evaluate(() => {
+        const data2 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(3) > td:nth-child(2)")
+        return data2.innerText
+    })
+
+    const resultado3 = await openpage.evaluate(() => {
+        const data3 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(4) > td:nth-child(2)")
+        return data3.innerText
+    })
+
+    const resultado4 = await openpage.evaluate(() => {
+        const data4 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(5) > td:nth-child(2)")
+        return data4.innerText
+    })
+
+    const resultado5 = await openpage.evaluate(() => {
+        const data5 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(6) > td:nth-child(2)")
+        return data5.innerText
+    })
+
+    const resultado6 = await openpage.evaluate(() => {
+        const data6 = document.querySelector("#ctl00_ContentPlaceHolder1_ReadingHistoryGrid > tbody > tr:nth-child(7) > td:nth-child(2)")
+        return data6.innerText
+    })
+
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    //impresión de resultado 
+    console.log(resultado1)
+    console.log(resultado2)
+    console.log(resultado3)
+    console.log(resultado4)
+    console.log(resultado5)
+    console.log(resultado6)
 
     await browser.close()
 }
